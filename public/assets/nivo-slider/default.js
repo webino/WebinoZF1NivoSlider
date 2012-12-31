eb9d778ccdcfcc335b37ff1ee2a4466393e9b13d
Peter Bacinsky <peter@bacinsky.sk>
2013-01-01 00:38:56 +0100

    Updated to newest version + added default.js

---
+++ public/assets/nivo-slider/default.js
@@ -0,0 +1,16 @@
+$(function($) {
+    $('#slider').nivoSlider({
+        effect:"random",
+        slices:15,
+        boxCols:8,
+        boxRows:4,
+        animSpeed:600,
+        pauseTime:5500,
+        startSlide:0,
+        directionNav:false,
+        controlNav:false,
+        controlNavThumbs:false,
+        pauseOnHover:true,
+        manualAdvance:false
+    });
+});
